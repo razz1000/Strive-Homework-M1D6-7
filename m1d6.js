19853e48fb9a791e392017b6e89914df626d3507
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-03-07 15:54:33 +0000

    Done with all assignments

---
+++ m1d6.js
@@ -0,0 +1,232 @@
+// JS Functions
+/* EXERCISE 1
+    Write a function called dice; it should randomize an integer number between 1 and 6.
+*/
+
+let dice = () => {
+  let theRandomNumber = Math.floor(Math.random() * 6) + 1;
+  console.log(theRandomNumber)
+}
+dice(); // it prints a random number each time in the Console.log 
+
+
+/* EXERCISE 2
+    Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
+    Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
+*/
+
+let splitMe = (str) => {
+  return str.split(' ');
+}
+
+splitMe("Hi there my name is Rasmus") // prints out :  ['Hi', 'there', 'my', 'name', 'is', 'Rasmus']
+
+
+
+/* EXERCISE 3
+    Write a function called deleteOne which receives a string and a boolean as parameters.
+    If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
+*/
+
+let deleteOne = (str, something) => {
+  if (str === something) {
+    return str.substring(1);
+  } else { 
+    return str.substring(0, str.length - 1);
+
+  }
+}
+deleteOne("awesomeness");
+
+
+
+/* EXERCISE 4
+    Write a function called rollTheDices which receives a number as a parameter.
+    It should invoke the dice() function defined in Ex1 the specified amount of times,
+    and return an object containing a sum property holding the sum of all values extracted
+    and a values array containing the single values of the dicerolls themselves.
+    Example: RollTheDices(3) => returns {
+        sum: 10
+        values: [3, 3, 4]
+    }
+*/
+
+// JS Arrays & Objects
+// NOTE: the movies array used in some exercises is defined at the end of this file
+
+/* EXERCISE 6
+   Write a function called deleteProp which receives an object and a string as parameters,
+   and returns the given object after deleting its property named as the given string.
+*/
+
+/* EXERCISE 7
+    Write a function called oldestMovie which finds the oldest movie in the provided movies array.
+*/
+
+/* EXERCISE 8
+    Write a function called countMovies which returns the number of movies contained in the provided movies array.
+*/
+
+/* EXERCISE 9
+    Write a function called onlyTheTitles which creates an array with just the titles of the movies contained in the provided movies array.
+*/
+
+/* EXERCISE 10
+   Write a function called onlyInThisMillennium which returns only the movies produced in this millennium from the provided movies array.
+*/
+
+/* EXERCISE 11
+    Write a function called getMovieById which receives an id as a parameter and returns the movie with the given id from the provided movies array.
+*/
+
+/* EXERCISE 12
+    Write a function called sumAllTheYears which returns the sum of all the years in which the movies in the provided movies array have been produced.
+*/
+
+/* EXERCISE 13
+    Write a function called searchByTitle which receives a string as a parameter and returns all the movies in the provided movies array which contain that string in the title.
+*/
+
+// [EXTRAS] JS Advanced
+
+/* EXERCISE 14
+  Create a function called "halfTree" which receives a number as a parameter and builds an "*" half tree with the given height.
+  Example:
+  halfTree(3)
+  *
+  **
+  ***
+*/
+
+/* EXERCISE 15 
+  Create a function called "tree" which receives a number as a parameter and builds an "*" tree with the given height.
+  Example: 
+  tree(3)
+    *  
+   *** 
+  *****
+*/
+
+/* EXERCISE 16
+  Create a function called "isItPrime" that receives a number as a parameter and returns true if the given number is a prime number.
+*/
+
+/* WHEN YOU ARE FINISHED
+ Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today's Eduflow.
+*/
+
+/* This movies array is used throughout the exercises. You're not supposed to alter it. */
+const movies = [
+  {
+    Title: "The Lord of the Rings: The Fellowship of the Ring",
+    Year: "2001",
+    imdbID: "tt0120737",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lord of the Rings: The Return of the King",
+    Year: "2003",
+    imdbID: "tt0167260",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lord of the Rings: The Two Towers",
+    Year: "2002",
+    imdbID: "tt0167261",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lord of War",
+    Year: "2005",
+    imdbID: "tt0399295",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lords of Dogtown",
+    Year: "2005",
+    imdbID: "tt0355702",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lord of the Rings",
+    Year: "1978",
+    imdbID: "tt0077869",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lord of the Flies",
+    Year: "1990",
+    imdbID: "tt0100054",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Lords of Salem",
+    Year: "2012",
+    imdbID: "tt1731697",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
+  },
+  {
+    Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+    Year: "1984",
+    imdbID: "tt0087365",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
+  },
+  {
+    Title: "Lord of the Flies",
+    Year: "1963",
+    imdbID: "tt0057261",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
+  },
+  {
+    Title: "The Avengers",
+    Year: "2012",
+    imdbID: "tt0848228",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+  },
+  {
+    Title: "Avengers: Infinity War",
+    Year: "2018",
+    imdbID: "tt4154756",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+  },
+  {
+    Title: "Avengers: Age of Ultron",
+    Year: "2015",
+    imdbID: "tt2395427",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+  },
+  {
+    Title: "Avengers: Endgame",
+    Year: "2019",
+    imdbID: "tt4154796",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+  },
+]
--- script.js
+++ script.js
@@ -6,3 +6,111 @@ geth1.style.color = "blue";
 
 
 
+//EX7: Write a function to change the h1 text to something else
+
+let h1Tag = document.getElementsByTagName("h1")[0];
+
+let functionToChangeH1 = (namechange) => {
+    h1Tag.innerText = namechange;
+}
+
+functionToChangeH1("Peters shop"); //Changes the H1 to Peters shop
+
+//END : EX7: Write a function to change the h1 text to something else
+
+
+// EX8: Write a function to change the page background color
+
+let pageBackground = document.getElementsByTagName("body")[0];
+
+let changeBackgroundColorOfBody = (color) => {
+    pageBackground.style.backgroundColor = color;
+}
+
+changeBackgroundColorOfBody("lightblue");
+
+
+// ENDEX8: Write a function to change the page background color
+
+
+//EX9: Write a function to change the footer address with a fake one
+
+let footerAddress = document.getElementsByClassName("footer-class")[0];
+
+let changeFooterAddressText = (footerText) => {
+    footerAddress.innerText = footerText
+}
+
+changeFooterAddressText("This is the new address");
+
+// END : EX9: Write a function to change the footer address with a fake one
+
+
+//EX10: Write a function to add a CSS class to every Amazon link
+
+let amazonlinks = document.getElementsByTagName("a");
+
+ let addClassToAmazonLinks = (something) => {
+    amazonlinks[0,1,2,3,4].classList.add(something);
+}
+
+addClassToAmazonLinks("something2");
+
+// END : EX10: Write a function to add a CSS class to every Amazon link
+
+//EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
+/* let ion = document.getElementsByClassName("amazon")
+
+let toggleImages = () => {
+    for (let i = 0; i < ion.length; i++) {
+        ion[i].style.display = "none";
+    }
+}
+ */
+
+function setImageVisible(amazon2) {
+    let img = document.querySelector(".amazon");
+    if (amazon2 === "hidden") {
+        img.style.visibility = "hidden"      
+    } else {
+        img.style.visibility = "visible"
+    }
+}
+
+setImageVisible("hidden")
+
+
+//END: EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
+
+
+//EX12: Write a function to color the price of the products in a different one every time it's invoked
+
+let cardColor = document.getElementsByClassName("price")[0,1,2,3,4];
+let cardColorChangeOnClick = () => {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+
+    cardColor.style.backgroundColor = color;
+}
+
+
+cardColorChangeOnClick();
+
+
+// END EX12: Write a function to color the price of the products in a different one every time it's invoked
+
+
+
+
+
+
+
+
+
+
+
+
+
